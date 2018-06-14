c173598e13a568e70d9da29ed1a5a61877e958b2
rcp-suresh <39515155+rcp-suresh@users.noreply.github.com>
2018-06-14 22:41:22 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,1 +1,8 @@
 scp -i /home/ubuntu/ec2key.pem -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/wipro-myweb/target/myapp-SNAPSHOT-1.0-1.1.4.war ubuntu@34.217.148.218:/opt/tomcat8/webapps/
+
+
+a=$(head -n 7 pom.xml | tail -n 1 |awk '{print $1}' | sed -e "s/<version>//" | sed 's/.\{10\}$//')
+echo $a
+rm -rf *.war
+wget http://18.236.64.135:8081/repository/maven-releases/in/mpcechno/myapp/$a/myapp-$a.war
+mv *.war myapp.war
